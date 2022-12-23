991df3ef577b1ca3c238b3534c7ea25948d7c11c
Federico Cau <116791048+fedecauwedy@users.noreply.github.com>
2022-12-23 09:29:25 +0100

    Add files via upload

---
+++ Alfabeto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// write in 2019 by maurizio.conti@fablabromagna.org
+
+public static class Alfabeto
+{
+    public static bool Verifica(string input)
+    {
+        string alfabeto = "abcdefghijklmnopqrstuvzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char i='a'; 
+        bool c=false;
+        for(int k=0; k<alfabeto.Length; k++)
+        {
+            if(i==alfabeto[k])
+            {
+               c=true;
+               break;  
+            }
+            else
+            {
+                c=false;
+            }
+        }
+
+        return c;
+    }
+}
